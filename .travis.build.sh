f2057c3a6ce7b49ee150eacdc3879e7b6a6a2ebe
Benjamin Chrétien <chretien@lirmm.fr>
2013-06-24 00:16:10 +0900

    Prepare the ground for some real unit tests.

--- .travis.build.sh
+++ .travis.build.sh
@@ -24,14 +24,17 @@ export CFLAGS="-I$install_dir/include -L$install_dir/lib"
 export CXXFLAGS="-I$install_dir/include -L$install_dir/lib"
 export PYTHONPATH="~/virtualenv/python$PYTHON_VERSION/lib/python$PYTHON_VERSION/site-packages"
 
+echo "gcc version: "
+g++ --version
+
 # Build package
 echo "Building UDT..."
 
-# move library and headers to appropriate location 
+# move library and headers to appropriate location
 cd ${root_dir}/udt/udt4
 
 echo "Installing package files..."
-make -j1                               
+make -j1
 cp src/libudt.so ${install_dir}/lib/
 cp src/udt.h ${install_dir}/include/udt/udt.h
 
@@ -43,6 +46,6 @@ echo "Installing PyUDT..." \
 python ${root_dir}/setup.py install --prefix="~/virtualenv/python$PYTHON_VERSION"
 
 echo "Running tests..."
-cd example
-./run_tests.sh
+cd test
+./run_tests.py -v
 cd ..
