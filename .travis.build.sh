957cffa2b3b64bbcf1f0e99f13700072d8ebe783
Benjamin Chrétien <chretien@lirmm.fr>
2013-06-25 20:12:37 +0900

    Clean .travis.build.sh + add tests for Python 2.6.
    Also using CMake-based install for now.

--- .travis.build.sh
+++ .travis.build.sh
@@ -22,13 +22,10 @@ export LD_LIBRARY_PATH="$install_dir/lib:$LD_LIBRARY_PATH"
 export PKG_CONFIG_PATH="$install_dir/lib/pkgconfig:$PKG_CONFIG_PATH"
 export CFLAGS="-I$install_dir/include -L$install_dir/lib"
 export CXXFLAGS="-I$install_dir/include -L$install_dir/lib"
-export PYTHONPATH="~/virtualenv/python$PYTHON_VERSION/lib/python$PYTHON_VERSION/site-packages:$PYTHONPATH"
+export PYTHONPATH="/home/travis/virtualenv/python$PYTHON_VERSION/lib/python$PYTHON_VERSION/site-packages:$PYTHONPATH"
 
 pythonsite_dir=`python -c "import sys, os; print os.sep.join(['lib', 'python' + sys.version[:3], 'site-packages'])"`
 export PYTHONPATH="$install_dir/$pythonsite_dir:$PYTHON_PATH"
-
-echo "Environment variables..."
-env
 
 echo "g++ version: "
 g++ --version
@@ -46,12 +43,23 @@ cp src/udt.h ${install_dir}/include/udt/udt.h
 
 cd ../..
 
-echo "Calling setup.py..."
-python ${root_dir}/setup.py build
-echo "Installing PyUDT..." \
-python ${root_dir}/setup.py install
+# Python-based install
+#echo "Calling setup.py..."
+#python ${root_dir}/setup.py build
+#echo "Installing PyUDT..." \
+#python ${root_dir}/setup.py install -v --prefix="${install_dir}"
 
-ls -R ${root_dir}/build
+# CMake-based install
+echo "Building Python bindings..."
+mkdir build && cd build
+cmake .. -DPythonInterp_FIND_VERSION=${PYTHON_VERSION} \
+         -DPythonLibs_FIND_VERSION=${PYTHON_VERSION} \
+         -DCMAKE_INSTALL_PREFIX=${install_dir} \
+         -DCMAKE_CXX_FLAGS="-I${install_dir}/include" \
+         -DCMAKE_MODULE_LINKER_FLAGS="-L${install_dir}/lib"
+make VERBOSE=1
+make install
+cd ..
 
 echo "Running tests..."
 ./test/run_tests.py -v
